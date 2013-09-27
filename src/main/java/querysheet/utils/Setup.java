65493cc0f76547c90cbd09bd788a4aad40d52df0
Fernando Ultremare <feroult@gmail.com>
2013-09-27 14:11:35 -0300

    extracting gapi

--- src/main/java/querysheet/utils/Setup.java
+++ src/main/java/querysheet/utils/Setup.java
@@ -2,14 +2,6 @@ package querysheet.utils;
 
 public class Setup {
 
-	public static String getServiceAccountEmail() {
-		return System.getenv("QS_SERVICE_ACCOUNT_EMAIL");
-	}
-
-	public static String getServiceAccountKeyPath() {
-		return System.getenv("QS_SERVICE_ACCOUNT_KEY_PATH");
-	}
-
 	public static String getJdbcUrl() {
 		return String.format("jdbc:postgresql://%s:%s/%s", System.getenv("QS_HOST"), System.getenv("QS_PORT"),
 				System.getenv("QS_DBNAME"));
